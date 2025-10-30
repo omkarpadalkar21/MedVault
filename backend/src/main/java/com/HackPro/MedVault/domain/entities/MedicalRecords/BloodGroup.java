961ab83fd66e04b6e1ea16a6b0648073f95aeb59
omkar <omkarpadalkar21@gmail.com>
2025-10-30 21:02:54 +0530

    Add Auth Controller and Service

---
+++ backend/src/main/java/com/HackPro/MedVault/domain/entities/MedicalRecords/BloodGroup.java
@@ -0,0 +1,12 @@
+package com.HackPro.MedVault.domain.entities.MedicalRecords;
+
+public enum BloodGroup {
+    O_POSITIVE,
+    O_NEGATIVE,
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE
+}
