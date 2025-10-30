b7a4b25605d506edda30e1162e43d8e82685785a
omkar <omkarpadalkar21@gmail.com>
2025-10-30 14:36:46 +0530

    Init backend

---
+++ backend/src/test/java/com/HackPro/MedVault/MedVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HackPro.MedVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
